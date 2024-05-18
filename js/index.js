ab77bf7f750a612e3063cd8734ea77421ec01d80
cherrycoder9 <cherrycoder9@gmail.com>
2024-05-18 18:24:02 +0900

    test

--- js/index.js
+++ js/index.js
@@ -1,2 +1,14 @@
-console.log("code master");
-console.log("code master");
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+    res.send("hello");
+});
+
+app.get('/user/:id', function (req, res) {
+    const q = req.params;
+    console.log(q);
+    res.send(q);
+});
+
+app.listen(3000);
