54ab73970cd165de31a3f6bc4cd3297eb850bd57
cherrycoder9 <cherrycoder9@gmail.com>
2024-07-01 00:57:49 +0900

    RustRover IDE Test

---
+++ rust/my_test/target/debug/deps/my_test.d
@@ -0,0 +1,5 @@
+C:\Users\codemaster\Documents\workspace\practice\rust\my_test\target\debug\deps\my_test.exe: src\main.rs
+
+C:\Users\codemaster\Documents\workspace\practice\rust\my_test\target\debug\deps\my_test.d: src\main.rs
+
+src\main.rs:
---
+++ rust/my_test/target/debug/my_test.d
@@ -0,0 +1,1 @@
+C:\Users\codemaster\Documents\workspace\practice\rust\my_test\target\debug\my_test.exe: C:\Users\codemaster\Documents\workspace\practice\rust\my_test\src\main.rs
