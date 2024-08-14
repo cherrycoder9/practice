3a1580d2064b20b3675467305ba312c77809c52a
cherrycoder9 <cherrycoder9@gmail.com>
2024-08-14 21:14:22 +0900

    변수 선언과 초기화 문제

---
+++ cpp/ex01-lv1/fist01/zero2.cc
@@ -0,0 +1,14 @@
+// 변수 선언과 초기화
+// 정수형 변수 a, b, c를 선언하고 각각 5, 10, 15로 초기화
+// 그 다음 세 변수의 합을 계산해 sum 변수에 저장하고 출력
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    int a = 5, b = 10, c = 15;
+    int sum = a + b + c;
+    cout << sum << endl;
+    return 0;
+}
