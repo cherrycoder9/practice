9e1d2d843245e190157858ef5aeca3d4187507df
cherrycoder9 <cherrycoder9@gmail.com>
2024-08-14 21:19:45 +0900

    여러 데이터 타입 변수 선언 문제

---
+++ cpp/ex01-lv1/fist01/zero4.cc
@@ -0,0 +1,21 @@
+// 여러 데이터 타입 변수 선언
+// 다음 조건을 만족하는 변수를 선언하고 초기화
+// 정수형 변수 x를 10으로 초기화
+// 실수형 변수 y를 20.5로 초기화
+// 문자형 변수 z를 'A'로 초기화
+// 각 변수들의 값을 출력
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    int x = 10;
+    float y = 20.5f;
+    char z = 'A';
+
+    cout << x << endl;
+    cout << y << endl;
+    cout << z << endl;
+    return 0;
+}
