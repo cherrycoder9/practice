17c1d94f062be6a5ca325a8e0d31875f3937e617
cherrycoder9 <cherrycoder9@gmail.com>
2024-08-14 21:16:51 +0900

    상수와 변수 사용 문제

---
+++ cpp/ex01-lv1/fist01/zero3.cc
@@ -0,0 +1,19 @@
+// 상수와 변수 사용
+// 정수형 상수 MAX 선언하고 100을 할당
+// 그 다음 정수형 변수 value를 선언하고 50으로 초기화
+// 그다음 value 값에 MAX를 더한 결과를 다시 value에 저장
+// 최종 값을 출력
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    int MAX = 100;
+    int value = 50;
+    value += MAX;
+
+    cout << value << endl;
+
+    return 0;
+}
