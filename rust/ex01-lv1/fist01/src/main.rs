ad9213db4dbc0daead8aa5d9b82c16be295259bf
cherrycoder9 <cherrycoder9@gmail.com>
2024-08-16 16:20:39 +0900

    타입 지정

--- rust/ex01-lv1/fist01/src/main.rs
+++ rust/ex01-lv1/fist01/src/main.rs
@@ -1,7 +1,9 @@
 mod zero1;
 mod zero2;
+mod zero3;
 
 fn main() {
     // zero1::declare_variable();
-    zero2::mutable_and_immutable_variables();
+    // zero2::mutable_and_immutable_variables();
+    zero3::specify_type();
 }
