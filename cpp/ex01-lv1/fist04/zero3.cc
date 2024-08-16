d6f854da0fefb74b61a5784fb74932397af65384
cherrycoder9 <cherrycoder9@gmail.com>
2024-08-16 11:54:16 +0900

    특정 숫자 건너뛰기

---
+++ cpp/ex01-lv1/fist04/zero3.cc
@@ -0,0 +1,16 @@
+// 특정 숫자 건너뛰기
+// 1~20까지 숫자 출력, 10의 배수는 건너뛰기
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    for (size_t i = 1; i <= 20; i++) {
+        if (i % 10 == 0) {
+            continue;
+        }
+        cout << i << endl;
+    }
+
+    return 0;
+}
