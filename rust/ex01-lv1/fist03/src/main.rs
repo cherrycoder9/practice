53bd42125fe59e6ee526c8fbf6ceb7e9d6ec1019
cherrycoder9 <cherrycoder9@gmail.com>
2024-08-18 19:11:03 +0900

    숫자 판단

--- rust/ex01-lv1/fist03/src/main.rs
+++ rust/ex01-lv1/fist03/src/main.rs
@@ -1,10 +1,13 @@
 use zero1::{ex_for, ex_loop, ex_match, ex_while, if_else};
+use zero2::check_number;
 
 mod zero1;
+mod zero2;
 fn main() {
-    if_else();
-    ex_match();
-    ex_while();
-    ex_for();
-    ex_loop();
+    // if_else();
+    // ex_match();
+    // ex_while();
+    // ex_for();
+    // ex_loop();
+    check_number(-3)
 }
