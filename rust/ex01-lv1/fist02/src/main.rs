6c3b6ad66adeb8d9e0830a485e20ba1830321ed2
cherrycoder9 <cherrycoder9@gmail.com>
2024-08-18 12:58:52 +0900

    불변 참조의 합 반환

--- rust/ex01-lv1/fist02/src/main.rs
+++ rust/ex01-lv1/fist02/src/main.rs
@@ -1,6 +1,12 @@
 mod zero1;
 mod zero2;
+mod zero3;
+
 fn main() {
     // zero1::immutable_and_mutable_references();
-    zero2::immutable_and_mutable_references2();
+    // zero2::immutable_and_mutable_references2();
+    let x = 5;
+    let y = 10;
+    let result = zero3::sum_values(&x, &y);
+    println!("x + y = {}", result)
 }
---
+++ rust/ex01-lv1/fist02/src/zero3.rs
@@ -0,0 +1,4 @@
+// 두 개의 불변 참조를 인자로 받아 그 합을 반환
+pub fn sum_values(a: &i32, b: &i32) -> i32 {
+    return a + b;
+}
