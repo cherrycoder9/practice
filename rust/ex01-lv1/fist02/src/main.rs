3a633490d6daeb0203b57e47e554cecffaf8553a
cherrycoder9 <cherrycoder9@gmail.com>
2024-08-18 13:38:31 +0900

    참조와 출력하기

--- rust/ex01-lv1/fist02/src/main.rs
+++ rust/ex01-lv1/fist02/src/main.rs
@@ -1,12 +1,26 @@
-mod zero1;
-mod zero2;
-mod zero3;
+// mod zero1;
+// mod zero2;
+// mod zero3;
+// mod zero4;
+mod zero5;
 
 fn main() {
     // zero1::immutable_and_mutable_references();
+
     // zero2::immutable_and_mutable_references2();
-    let x = 5;
-    let y = 10;
-    let result = zero3::sum_values(&x, &y);
-    println!("x + y = {}", result)
+
+    // let x = 5;
+    // let y = 10;
+    // let result = zero3::sum_values(&x, &y);
+    // println!("x + y = {}", result);
+
+    // let mut number = 8;
+    // zero4::update_value(&mut number);
+    // println!("number: {}", number);
+
+    let x = 3;
+    let y = 7;
+    let mut sum = 0;
+    zero5::print_values(&x, &y, &mut sum);
+    println!("x: {}, y: {}, sum: {}", x, y, sum);
 }
