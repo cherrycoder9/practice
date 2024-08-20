3b8b89b436792041ada4417b8c8aa2d65c316f76
cherrycoder9 <cherrycoder9@gmail.com>
2024-08-20 10:11:05 +0900

    튜플 구조체

--- rust/ex01-lv1/fist04/src/main.rs
+++ rust/ex01-lv1/fist04/src/main.rs
@@ -1,8 +1,10 @@
 use zero1::Person;
 use zero2::Car;
+use zero3::Color;
 
 mod zero1;
 mod zero2;
+mod zero3;
 fn main() {
     // 구조체 인스턴스 생성
     let mut p = Person {
@@ -26,4 +28,9 @@ fn main() {
     // 연관 함수 호출로 구조체 인스턴스 생성
     let c = Car::new(1986, String::from("KIA"), String::from("Avantte"));
     println!("년도: {}, 회사: {}, 이름: {}", c.year, c.company, c.name);
+
+    let black = Color(0, 0, 0);
+
+    // 필드에 인덱스로 접근
+    println!("Red: {}, Green: {}, Blue: {}", black.0, black.1, black.2);
 }
