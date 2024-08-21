33dfc8938de4659d2aab7b08354415b19245b01a
cherrycoder9 <cherrycoder9@gmail.com>
2024-08-21 16:48:46 +0900

    Result 열거형

--- rust/ex01-lv1/fist05/src/main.rs
+++ rust/ex01-lv1/fist05/src/main.rs
@@ -2,9 +2,11 @@ mod zero1;
 mod zero2;
 mod zero3;
 mod zero4;
+mod zero5;
 fn main() {
     // zero1::ex_enum();
     // zero2::ex_enum2();
     // zero3::ex_enum3();
-    zero4::ex_enum4();
+    // zero4::ex_enum4();
+    zero5::ex_enum5();
 }
--- rust/ex01-lv1/fist05/src/zero5.rs
+++ rust/ex01-lv1/fist05/src/zero5.rs
@@ -1,1 +1,13 @@
 // Result 열거형
+// 작업이 성공할 때 Ok, 실패할 때 Err 값을 반환
+// Result는 파일 I/O, 네트워킹 등의 작업에서 에러 처리시 자주 사용
+fn read_file(filename: &str) -> Result<String, std::io::Error> {
+    std::fs::read_to_string(filename)
+}
+
+pub fn ex_enum5() {
+    match read_file("hello.txt") {
+        Ok(contents) => println!("파일 내용: {}", contents),
+        Err(e) => println!("파일 읽는 중 오류 발생: {}", e),
+    }
+}
