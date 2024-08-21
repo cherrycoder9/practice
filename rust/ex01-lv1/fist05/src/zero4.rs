495c4634fa14a70b0d3ac84e4082ae4acad5c520
cherrycoder9 <cherrycoder9@gmail.com>
2024-08-21 16:44:24 +0900

    Option 열거형

--- rust/ex01-lv1/fist05/src/zero4.rs
+++ rust/ex01-lv1/fist05/src/zero4.rs
@@ -1,6 +1,6 @@
-// 열거형과 Option, Result
-// 일반적인 에러 처리와 선택적인 값 처리를 위해 사용
 // Option 열거형
+// 값이 있을수도 있고 없을수도 있는 경우를 표현함
+// Some은 값이 있을때, None은 값이 없을때 사용
 fn divide(numerator: f64, denominator: f64) -> Option<f64> {
     if denominator == 0.0 {
         None
