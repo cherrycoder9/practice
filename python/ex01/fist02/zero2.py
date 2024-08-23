f23cdf887dee18eb344cc2d4315a9171aa2a6895
cherrycoder9 <cherrycoder9@gmail.com>
2024-08-23 22:38:26 +0900

    리스트 병합과 반복

---
+++ python/ex01/fist02/zero2.py
@@ -0,0 +1,13 @@
+# 리스트 병합과 반복
+# 새로운 리스트 생성시 유용한 방법
+
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+
+# 리스트 병합
+merged_list = list1 + list2
+print(merged_list)
+
+# 리스트 반복
+repeated_list = list1 * 3
+print(repeated_list)
