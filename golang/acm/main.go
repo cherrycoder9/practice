23abbdb669986d79b078b09e423f1972ec9a37fb
cherrycoder <cherrycoder9@gmail.com>
2025-01-04 08:30:59 +0900

    ...

--- golang/acm/main.go
+++ golang/acm/main.go
@@ -6,5 +6,17 @@ func main() {
 	var a, b int
 	_, _ = fmt.Scan(&a, &b)
 
-	fmt.Println(a + b)
+	if a > 0 {
+		if b > 0 {
+			fmt.Println("1")
+		} else {
+			fmt.Println("4")
+		}
+	} else {
+		if b > 0 {
+			fmt.Println("2")
+		} else {
+			fmt.Println("3")
+		}
+	}
 }
--- rust/acm/src/main.rs
+++ rust/acm/src/main.rs
@@ -2,16 +2,16 @@ use std::io;
 
 fn main() {
     let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Failed to read line");
+    io::stdin().read_line(&mut input).expect("");
+    let x: i32 = input.trim().parse().expect("");
     
-    let nums: Vec<i32> = input
-        .trim() // 공백 제거
-        .split_whitespace() // 공백으로 분리
-        .map(|x| x.parse::<i32>().expect("Invalid input"))// 문자열 -> 정수 변환 
-        .collect(); // 벡터로 수집 
+    input.clear();
+    io::stdin().read_line(&mut input).expect("");
+    let y: i32 = input.trim().parse().expect("");
     
-    let a = nums[0];
-    let b = nums[1];
-    
-    println!("{}", a + b); 
+    if x > 0 { 
+        if y > 0 { println!("1") } else { println!("4"); }
+    } else { 
+        if y > 0 { println!("2") } else { println!("3"); }
+    }
 }
