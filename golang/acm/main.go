42ee321b60f4b84f5a3b78e11b12ef2d56603074
cherrycoder <cherrycoder9@gmail.com>
2025-01-08 23:42:34 +0900

    ...

--- golang/acm/main.go
+++ golang/acm/main.go
@@ -6,12 +6,11 @@ import (
 )
 
 func main() {
-	text := "Hello World!"
-	substr := "Wor"
+	text := "apple,orange,banana"
+	separator := ","
 
-	if strings.Contains(text, substr) {
-		fmt.Println("문자열에 포함되어 있습니다.")
-	} else {
-		fmt.Println("문자열에 포함되어 있지 않습니다.")
-	}
+	// 문자열 분리
+	parts := strings.Split(text, separator)
+
+	fmt.Println(parts) // [apple orange banana]
 }
