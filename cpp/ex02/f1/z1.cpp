3d07c19954eee397b0a177bc921870cb859549f6
cherrycoder <cherrycoder9@gmail.com>
2025-01-16 22:47:19 +0900

    벡터와 문자열 객체 생성

---
+++ cpp/ex02/f1/z1.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+#include <string>
+#include <iostream>
+
+int main() {
+    using std::vector, std::string, std::cout;
+    const vector<int> x{1, 2, 3};
+    const string s{"Vector: "};
+    cout << s << x[0] << ", " << x[1] << ", " << x[2] << "\n";
+
+    return 0;
+}
